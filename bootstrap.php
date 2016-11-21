6392b661c58077bee657556570316aecbd62a8c4
omerida <oscar@oscarm.org>
2016-11-21 15:54:26 -0500

    Adding bootstrap.php input handling. Value object needs getters.

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,20 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 require __DIR__ . '/vendor/autoload.php';
+
+use Guzzle\Service\Client;
+
+$text = $_POST['text'];
+$token = $_POST['token'];
+
+$secret = 'enter_your_token';
+
+if ($token !== $secret) {
+   $msg = "The token for the slash command doesn't match. Check your script.";
+   header('HTTP/1.0 401 Unauthorized');
+   die($msg);
+}
+
+echo (new \ChatBot\PhpManualChatBot(new Client()))
+    ->lookupFunction($text)
+    ->getMethodDescription();
